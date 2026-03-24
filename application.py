62be1b45e2db5ea6b52ca7c392f16231743f2c10
yashh-alpha <kyash4886@gmail.com>
2026-03-24 14:34:47 +0530

    removed the index page

--- application.py
+++ application.py
@@ -13,11 +13,8 @@ app = application
 ridge_model = pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler = pickle.load(open("models/scaler.pkl",'rb'))
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
-@app.route("/predictdata", methods = ['GET','POST'])
+@app.route("/", methods = ['GET','POST'])
 def predict_datapoint():
     if request.method == "POST":
         temp = float(request.form["temperature"])
