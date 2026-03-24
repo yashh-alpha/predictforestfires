6dcc084e81e3db9ff0c5aca8bacf7519a04ac8dc
yashh-alpha <kyash4886@gmail.com>
2026-03-24 14:48:02 +0530

    removed index.html

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ standard_scaler = pickle.load(open("models/scaler.pkl",'rb'))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/predictdata", methods = ['GET','POST'])
 def predict_datapoint():
